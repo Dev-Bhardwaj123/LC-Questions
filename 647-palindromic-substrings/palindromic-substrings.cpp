1b2df28dd0bf3d23c8e728179d2b3b24d00c7d58
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2024-11-06 23:42:15 +0530

    Time: 27 ms (41.95%) | Memory: 11.6 MB (51.89%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n=s.length(),ans=0;
-        for(int i=0;i<n;i++){
-            int even=palindromeCount(s,i,i+1);
-            int odd=palindromeCount(s,i,i);
-            ans+=even+odd;
+        int n = s.length();
+        vector<vector<bool>> palindrome(n, vector<bool>(n, false));
+        int ans = 0;
+
+        for (int i = 0; i < n; ++i) {
+            palindrome[i][i] = true;
+            ans++;
         }
+
+        for (int i = 0; i < n - 1; ++i) {
+            if (s[i] == s[i + 1]) {
+                palindrome[i][i + 1] = true;
+                ans++;
+            }
+        }
+
+        for (int len = 3; len <= n; ++len) {
+            for (int i = 0; i < n - len + 1; ++i) {
+                if (s[i] == s[i + len - 1] && palindrome[i + 1][i + len - 2]) {
+                    palindrome[i][i + len - 1] = true;
+                    ans++;
+                }
+            }
+        }
+
         return ans;
-    }
-    int palindromeCount(const string& s,int left,int right){
-        int cnt=0;
-        while(left>=0 && right<s.length() && s[left]==s[right]){
-            --left;
-            ++right;
-            ++cnt;
-        }
-        return cnt;
     }
 };
