0b06cfb6939006d823702796c5d0b5c1c571c922
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2024-11-13 16:49:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (49.18%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,48 +1,44 @@
 class Solution {
 public:
+    bool valid(int i,int j,vector<vector<int>>& grid){
+        return i>=0&&i<grid.size()&&j>=0&&j<grid[0].size()&&grid[i][j]==1;
+    }
+    int dir[4][2]={{0,1},{1,0},{-1,0},{0,-1}};
     int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        //{{row,col},time}
-        queue<pair<pair<int,int>,int>> q;
-        int vis[n][m];
-        int cntFresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        int cnt=0;
+        queue<pair<int,int>> q;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
                 if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                }
-                else{
-                    vis[i][j]=0;
+                    q.push({i,j});
                 }
             }
         }
-        int tm=0;
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
         while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
-            tm=max(tm,t);
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=r+delrow[i];
-                int ncol=c+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol},t+1});
-                    vis[nrow][ncol]=2;
+            int c=q.size();
+            cnt++;
+            while(c--){
+                auto d=q.front();
+                int x=d.first;
+                int y=d.second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int new_x=x+dir[i][0];
+                    int new_y=y+dir[i][1];
+                    if(valid(new_x,new_y,grid)){
+                        grid[new_x][new_y]=2;
+                        q.push({new_x,new_y});
+                    }
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
+        for(auto i:grid){
+            for(auto j:i){
+                if(j==1){
                     return -1;
                 }
             }
         }
-        return tm;
+        return cnt==0?0:cnt-1;
     }
 };
