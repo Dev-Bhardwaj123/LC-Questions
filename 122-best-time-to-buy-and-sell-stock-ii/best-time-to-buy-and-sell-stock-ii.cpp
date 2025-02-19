f161a9f3af474d97b0dc8722beddcd1f87f5a665
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-02-19 20:20:22 +0530

    Time: 3 ms (31.10%) | Memory: 22.2 MB (7.36%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
-    int f(int ind,int buy,vector<int>& prices,vector<vector<int>>& dp){
-        if(ind==prices.size()){
+    int solve(int i,int buy,vector<int>& prices,vector<vector<int>>& dp){
+        //base case
+        if(i==prices.size()){
             return 0;
         }
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
         }
-        int profit=0;
+        int prof=0;
+        //if bought then only one can sell
         if(buy){
-            profit=max(-prices[ind]+f(ind+1,0,prices,dp),0+f(ind+1,1,prices,dp));
+            //maximize profit 
+            //if bought then check case of selling and not selling
+            prof=max(-prices[i]+solve(i+1,0,prices,dp),solve(i+1,1,prices,dp));
         }
         else{
-            profit=max(prices[ind]+f(ind+1,1,prices,dp),0+f(ind+1,0,prices,dp));
+            //if not bought check case of buying and not buying
+            prof=max(prices[i]+solve(i+1,1,prices,dp),solve(i+1,0,prices,dp));
         }
-        return dp[ind][buy]=profit;
+        dp[i][buy]=prof;
+        return dp[i][buy];
     }
     int maxProfit(vector<int>& prices) {
         vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return f(0,1,prices,dp);
+        int z=solve(0,1,prices,dp);
+        return z;
     }
 };
