64fb41200696e21ab019e6b703332d54c1878c75
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-02-28 19:15:34 +0530

    Time: 66 ms (27.07%) | Memory: 61.8 MB (42.05%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isPalindrome(string s,int start,int end){
+        while(start<=end){
+            if(s[start++]!=s[end--]){
+                return false;
+            }
+        }
+        return true;
+    }
+    void solve(int ind,vector<string>& temp,string s,vector<vector<string>>& ans){
+        //base case when partition bar reaches out of length
+        if(ind==s.length()){
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=ind;i<s.length();i++){
+            if(isPalindrome(s,ind,i)){
+                temp.push_back(s.substr(ind,i-ind+1));
+                solve(i+1,temp,s,ans);
+                temp.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0,temp,s,ans);
+        return ans;
+
+    }
+};
