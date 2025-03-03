0218d439990cf5f6925f51443b9a36ce090a1997
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-03-03 13:20:29 +0530

    Time: 3 ms (7.46%) | Memory: 10.1 MB (5.17%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,13 +3,12 @@ public:
     bool isValid(string s) {
         stack<int> st;
         for(int i=0;i<s.length();i++){
-            if(!st.empty()&&((st.top()=='(' && s[i]==')')||(st.top()=='{' && s[i]=='}')||(st.top()=='[' && s[i]==']'))){
+            if(!st.empty()&&((st.top()=='(' && s[i]==')')||(st.top()=='[' && s[i]==']')||(st.top()=='{' && s[i]=='}'))){
                 st.pop();
+                continue;
             }
-            else{
-                st.push(s[i]);
-            }
+            st.push(s[i]);
         }
-        return st.size()==0;
+        return st.empty();
     }
 };
