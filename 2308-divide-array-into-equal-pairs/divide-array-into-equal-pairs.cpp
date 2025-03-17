459a20fd458a83073d47a439ae4d4975f6b2d14c
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-03-17 09:31:18 +0530

    Time: 7 ms (19.84%) | Memory: 18.1 MB (49.04%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -5,11 +5,7 @@ public:
         for(int i=0;i<nums.size();i++){
             mp[nums[i]]++;
         }
-        int k=0;
         for(auto m:mp){
-            if(m.second%2==0){
-                k+=(m.second/2);
-            }
             if(m.second%2!=0){
                 return false;
             }
