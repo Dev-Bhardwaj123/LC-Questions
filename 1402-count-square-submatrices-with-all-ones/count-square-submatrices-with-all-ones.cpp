633f1b36b1804b970fb7a465131f246436ef97a2
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-03-17 19:19:19 +0530

    Time: 21 ms (10.82%) | Memory: 31.7 MB (5.41%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& matrix,vector<vector<int>>& dp){
-        if (i<0||j<0|| matrix[i][j]==0 ) return 0;
-        if (dp[i][j]!=-1) return dp[i][j];
-        return dp[i][j]=1+min({f(i-1, j, matrix,dp),f(i-1, j-1, matrix,dp),f(i, j-1, matrix,dp)});
+    int solve(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& dp){
+        //base case
+        if(i<0 || j<0 || matrix[i][j]==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        dp[i][j]=1+min({solve(i-1,j,matrix,dp),solve(i-1,j-1,matrix,dp),solve(i,j-1,matrix,dp)});
+        return dp[i][j];
     }
     int countSquares(vector<vector<int>>& matrix) {
-        const int r=matrix.size(), c=matrix[0].size();
+        int r=matrix.size(),c=matrix[0].size();
         vector<vector<int>> dp(r,vector<int>(c,-1));
         int cnt=0;
-        for(int i=0; i<r; i++)
-            for(int j=0; j<c; j++)
-                cnt+=f(i, j, matrix,dp);
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                cnt+=solve(i,j,matrix,dp);
+            }
+        }
         return cnt;
     }
 };
