35498d6d7bcb61ad2265a52105ab78b3ac6939c7
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-03-18 14:45:26 +0530

    Time: 303 ms (5.01%) | Memory: 402.9 MB (5.21%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int solve(int i,int j,string word1,string word2,vector<vector<int>>& dp){
-        if (i >= word1.length()) return word2.length() - j;
-        if (j >= word2.length()) return word1.length() - i; 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        //if match then move ahead
+        if(i>=word1.length()) return word2.length()-j;
+        if(j>=word2.length()) return word1.length()-i;
         if(word1[i]==word2[j]){
             return dp[i][j]=solve(i+1,j+1,word1,word2,dp);
         }
-        //insert
-        int ins=1+solve(i,j+1,word1,word2,dp);
-        //delete
-        int del=1+solve(i+1,j,word1,word2,dp);
-        //replace
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int inc=1+solve(i,j+1,word1,word2,dp);
+        int dec=1+solve(i+1,j,word1,word2,dp);
         int rep=1+solve(i+1,j+1,word1,word2,dp);
-
-        return dp[i][j]=min(ins,min(del,rep));
+        return dp[i][j]=min({inc,dec,rep});
     }
     int minDistance(string word1, string word2) {
+        int n=word1.length(),m=word2.length();
         vector<vector<int>> dp(word1.length(),vector<int>(word2.length(),-1));
-        int z=solve(0,0,word1,word2,dp);
-        return z;
+        return solve(0,0,word1,word2,dp);
+
     }
 };
