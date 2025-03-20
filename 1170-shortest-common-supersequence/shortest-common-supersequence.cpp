420a9352166a2f3d278943d8d1dcc863b255bc52
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-03-20 15:48:45 +0530

    Time: 24 ms (63.82%) | Memory: 26.9 MB (38.16%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int m = str1.length();
-        int n = str2.length();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        int n=str1.length();
+        int m=str2.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1]==str2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        int i = m, j = n;
-        string result = "";
-        
-        while (i > 0 && j > 0) {
-            if (str1[i - 1] == str2[j - 1]) {
-                result.push_back(str1[i - 1]);
+        string ans="";
+        int i=n,j=m;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans+=str1[i-1];
                 i--;
                 j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                result.push_back(str1[i - 1]);
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                ans+=str1[i-1];
                 i--;
-            } else {
-                result.push_back(str2[j - 1]);
+            }
+            else{
+                ans+=str2[j-1];
                 j--;
             }
         }
-        while (i > 0) {
-            result.push_back(str1[i - 1]);
+        while(i>0){
+            ans+=str1[i-1];
             i--;
         }
-        while (j > 0) {
-            result.push_back(str2[j - 1]);
+        while(j>0){
+            ans+=str2[j-1];
             j--;
         }
-        reverse(result.begin(), result.end());
-        
-        return result;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
