7d254164c8fde6487a0ee483962abff6d97dfc66
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-03-24 09:16:48 +0530

    Time: 68 ms (64.63%) | Memory: 126.6 MB (59.18%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        sort(meetings.begin(),meetings.end());
+        vector<vector<int>>arr;
+        arr.push_back(meetings[0]);
+        for(int j=1;j<meetings.size();j++){
+            if(meetings[j][0]<=arr.back()[1]){
+                arr.back()[1]=max(arr.back()[1],meetings[j][1]);
+            }
+            else{
+                arr.push_back(meetings[j]);
+            }
+        }
+        int count=0;
+        for(int i=0;i<arr.size();i++){
+           count+=(arr[i][1]-arr[i][0]+1);
+        }
+        return (days-count);
+    }
+};
