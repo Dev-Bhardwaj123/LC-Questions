4c9e07d6b3d6a7dc4316473d5bb09cdf9700f124
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-04-26 12:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (89.39%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int orColor=image[sr][sc];
-        if(orColor==color) return image;
-        image[sr][sc]=color;
+    void dfs(int row,int col,vector<vector<int>>& image,int newColor,int drow[],int dcol[],int ini){
+        image[row][col]=newColor;
         int n=image.size();
         int m=image[0].size();
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
-        int dr[]={-1,0,1,0};
-        int dc[]={0,-1,0,1};
-        while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
-            q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=r+dr[i];
-                int ncol=c+dc[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==orColor){
-                    image[nrow][ncol]=color;
-                    q.push({nrow,ncol});
-                }
+        for(int i=0;i<4;i++){
+            int nrow=row+drow[i];
+            int ncol=col+dcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==ini){
+                dfs(nrow,ncol,image,newColor,drow,dcol,ini);
             }
         }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int ini=image[sr][sc];
+        if(ini==color) return image;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        dfs(sr,sc,image,color,drow,dcol,ini);
         return image;
     }
 };
