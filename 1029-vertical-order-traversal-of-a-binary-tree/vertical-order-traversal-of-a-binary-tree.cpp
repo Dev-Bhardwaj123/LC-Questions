e740a1b16280cd087fc53d51ae77b11ab69798ea
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-04-29 19:58:32 +0530

    Time: 4 ms (22.79%) | Memory: 15.4 MB (96.15%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,34 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector< vector<int>>ans;
-        if(root==nullptr) {
+        vector<vector<int>> ans;
+        if(root==nullptr){
             return ans;
         }
-        map<int,vector<pair<int,int>>>m;
-        queue<pair<TreeNode*,pair<int,int>>>q;
+        map<int,vector<pair<int,int>>> mp;
+        queue<pair<TreeNode*,pair<int,int>>> q;
         q.push({root,{0,0}});
         while(!q.empty()){
             auto it=q.front();
+            TreeNode* curr=it.first;
+            int line=it.second.first;
+            int lvl=it.second.second;
             q.pop();
-            TreeNode* node = it.first;
-            int line = it.second.first;
-            int level = it.second.second;
-            m[line].emplace_back(level,node->val);
-            if(node->left!=NULL){
-                q.push({node->left,{line-1,level+1}});
+            mp[line].emplace_back(lvl,curr->val);
+            if(curr->left!=nullptr){
+                q.push({curr->left,{line-1,lvl+1}});
             }
-             if(node->right!=NULL){
-                q.push({node->right,{line+1,level+1}});
+            if(curr->right!=nullptr){
+                q.push({curr->right,{line+1,lvl+1}});
             }
         }
-        for(auto it:m){
-            vector<int>level;
-            sort(it.second.begin(),it.second.end());
-            for(auto p : it.second){
-            level.push_back(p.second);
+        for(auto& m:mp){
+            vector<int> lev;
+            sort(m.second.begin(),m.second.end());
+            for(auto& p:m.second){
+                lev.push_back(p.second);
             }
-            ans.push_back(level);
+            ans.push_back(lev);
         }
         return ans;
     }
