2776629bed72f60a30d17909d7db76c10014090e
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-06-23 08:53:06 +0530

    Time: 3 ms (32.73%) | Memory: 15.7 MB (11.45%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void sub(int ind,vector<int>& nums,vector<int>& temp,vector<vector<int>>& ans){
-        //base case
-        if(ind ==nums.size()){
-            ans.push_back(temp);
+    void solve(int i,vector<int> temp,vector<vector<int>>& res,vector<int>& nums){
+        if(i==nums.size()){
+            res.push_back(temp);
             return;
         }
-        temp.push_back(nums[ind]); //take the element
-        sub(ind+1,nums,temp,ans); //move further
-        temp.pop_back(); //backtrack and remove
-        sub(ind+1,nums,temp,ans); //move ahead once not taken
+        temp.push_back(nums[i]);
+        solve(i+1,temp,res,nums);
+        temp.pop_back();
+        solve(i+1,temp,res,nums);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> temp;
-        sub(0,nums,temp,ans);
-        return ans;
+        vector<vector<int>> res;
+        vector<int> temp;   
+        solve(0,temp,res,nums);
+        return res;
     }
 };
