3a08426dfd0d4033f212494a1a4e62a2f1957797
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-06-29 10:10:30 +0530

    Time: 2176 ms (5.05%) | Memory: 7.9 MB (61.45%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
-    int dp[20]{};
     int numTrees(int n) {
-        if(n<=1){
-            return 1;
+        if(n<=1) return 1;
+        int res=0;
+        for(int i=0;i<n;i++){
+            res+=numTrees(i)*numTrees(n-i-1);
         }
-        if(dp[n]){
-            return dp[n];
-        }
-        for(int i=1;i<=n;i++){
-            dp[n]+=numTrees(i-1)*numTrees(n-i);
-        }
-        return dp[n];
+        return res;
     }
 };
