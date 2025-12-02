1b7e0308922ff4f153e2fd7a0644e5185d576484
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-12-02 11:04:02 +0530

    Time: 0 ms (100.00%) | Memory: 32.5 MB (80.87%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
-    int lessEqual(vector<int>&nums,int goal){
-        int l=0,r=0,sum=0,cnt=0;
-        if(goal<0){
-            return 0;
-        }
+    int sumLessthanEq(vector<int>& nums,int goal){
+        if(goal<0) return 0;
+        int l=0,r=0,sum=0;
+        int cnt=0;
         while(r<nums.size()){
             sum+=nums[r];
             while(sum>goal){
-                sum-=nums[l];
+                sum=sum-nums[l];
                 l++;
             }
             cnt+=(r-l+1);
@@ -17,6 +16,7 @@ public:
         return cnt;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        return lessEqual(nums,goal)-lessEqual(nums,goal-1);
+        int res=sumLessthanEq(nums,goal)-sumLessthanEq(nums,goal-1);
+        return res;
     }
 };
