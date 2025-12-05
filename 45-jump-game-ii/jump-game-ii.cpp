9362fedb4913fcee18a1abe0c586b37d309de0d1
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2025-12-05 11:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (84.59%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n=nums.size();
         int jumps=0;
         int l=0,r=0;
-        while(r<n-1){
+        while(r<nums.size()-1){
             int farthest=0;
             for(int i=l;i<=r;i++){
-                farthest=max(farthest,nums[i]+i);
+                farthest=max(i+nums[i],farthest);
             }
             l=r+1;
             r=farthest;
