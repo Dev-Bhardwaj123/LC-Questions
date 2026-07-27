23feaca38f04ab7cbd35dc9c585e3aa6a4d85d74
Dev-Bhardwaj123 <119854134+Dev-Bhardwaj123@users.noreply.github.com>
2026-07-27 06:20:25 +0530

    Time: 7 ms (32.43%) | Memory: 18.5 MB (43.73%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        for(auto pre:prerequisites){
-            adj[pre[1]].push_back(pre[0]);
+        int n=numCourses;
+        vector<int> adj[n];
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
         }
-        vector<int> indegree(adj.size(),0);
-        for(int i=0;i<adj.size();i++){
-            for(auto k:adj[i]){
-                indegree[k]++;
+        vector<int> indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it: adj[i]){
+                indegree[it]++;
             }
         }
         queue<int> q;
-        for(int i=0;i<adj.size();i++){
+        for(int i=0;i<n;i++){
             if(indegree[i]==0){
                 q.push(i);
             }
@@ -24,12 +25,10 @@ public:
             topo.push_back(node);
             for(auto it:adj[node]){
                 indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
-                }
+                if(indegree[it]==0) q.push(it);
             }
         }
-        if(topo.size()==numCourses) return topo;
-    else return {};
+        if(topo.size()==n) return topo;
+        return {};
     }
 };
